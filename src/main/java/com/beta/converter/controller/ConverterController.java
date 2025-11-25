63ab071ff7913ce45ad6e13a456838caf975a942
IvanDrv <ivander05@mail.ru>
2025-11-25 19:51:34 +0300

    Basic test methods

--- src/main/java/com/beta/converter/controller/ConverterController.java
+++ src/main/java/com/beta/converter/controller/ConverterController.java
@@ -1,7 +1,24 @@
 package com.beta.converter.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.beta.converter.dto.Lengths;
+import org.springframework.web.bind.annotation.*;
+
+//curl -X POST -d {\"number\":\"10\",\"inUnits\":\"KM\",\"outUnits\":\"MM\"} -H "Content-Type: application/json" http://localhost:8080/convert/test
 
 @RestController
+@CrossOrigin("*")
+@RequestMapping("/convert")
 public class ConverterController {
+
+    @PostMapping("/test")
+    private Lengths testPost(@RequestBody Lengths inputValues){
+        System.out.println(inputValues);
+        return inputValues;
+    }
+
+    @GetMapping("/test")
+    private String testGet(){
+        return "Тестовый GET работает!";
+    }
+
 }
